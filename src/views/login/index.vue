ba71cac76410c59bc801ddd02b6a18586dcc2e72
unknown <you@example.com>
2019-10-26 20:49:51 +0800

    登录页面初始化匹配输入信息

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,11 +4,11 @@
         <el-card>
           <img src="../../assets/logo_index.png" alt="">
 
-             <el-form :model="LoginForm">
-        <el-form-item>
+        <el-form :model="LoginForm" :rules="rules" status-icon>
+        <el-form-item  prop="mobile">
           <el-input v-model="LoginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item  prop="code">
           <el-input v-model="LoginForm.code" placeholder="请输入验证码" style="width:240px;margin-right:8px"></el-input>
           <el-button>发送验证码</el-button>
         </el-form-item>
@@ -26,12 +26,32 @@
 <script>
 export default {
   data () {
+    var rulezz = (rule, value, callback) => {
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('输入出错啦！'))
+      }
+    }
     return {
       LoginForm: {
         mobile: '',
         code: ''
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: rulezz, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '验证码是6位', trigger: 'blur' }
+        ]
       }
     }
+  },
+  methods: {
+
   }
 }
 </script>
@@ -40,7 +60,6 @@ export default {
     width: 100%;
     height: 100%;
     background:url(../../assets/login_bg.jpg) no-repeat center/cover;
-    /* background-color: red; */
     position: absolute;
     left: 0;
     top: 0;
--- src/views/login/text.vue
+++ src/views/login/text.vue
@@ -26,12 +26,10 @@
     <img src="../../assets/welcome.jpg" alt="">
   </el-header>
   <el-col :span="12">
-    <h5>自定义颜色</h5>
+    <!-- <h5>自定义颜色</h5> -->
     <el-menu
       default-active="2"
       class="el-menu-vertical-demo"
-      @open="handleOpen"
-      @close="handleClose"
       background-color="#545c64"
       text-color="#fff"
       active-text-color="#ffd04b">
@@ -41,27 +39,54 @@
           <span>导航一</span>
         </template>
         <el-menu-item-group>
-                   <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
         <el-menu-item index="1-3">选项3</el-menu-item>
-        <el-submenu index="1-4">
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
+        </el-menu-item-group>
       </el-submenu>
        <el-submenu index="2">
         <template slot="title">
           <i class="el-icon-location"></i>
-          <span>导航一</span>
+          <span>导航二</span>
         </template>
         <el-menu-item-group>
-                   <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-1">选项1</el-menu-item>
           <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
         </el-menu-item-group>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-        <el-submenu index="2-4">
-          <el-menu-item index="2-4-1">选项1</el-menu-item>
-        </el-submenu>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航三</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="3-1">选项1</el-menu-item>
+          <el-menu-item index="3-2">选项2</el-menu-item>
+          <el-menu-item index="3-3">选项3</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航四</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="4-1">选项1</el-menu-item>
+          <el-menu-item index="4-2">选项2</el-menu-item>
+          <el-menu-item index="4-3">选项3</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="5">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航五</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="5-1">选项1</el-menu-item>
+          <el-menu-item index="5-2">选项2</el-menu-item>
+          <el-menu-item index="5-3">选项3</el-menu-item>
+        </el-menu-item-group>
       </el-submenu>
     </el-menu>
   </el-col>
@@ -105,5 +130,8 @@ export default {
 .el-card{
     width: 400px;
     height: 350px;
+}
+.el-menu{
+    width: 200px;
 }
 </style>
